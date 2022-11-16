7276e98e2db464c95ebfcc750a0136a0fe859d53
cristiaanh <christiaan.holeman@gmail.com>
2022-11-16 13:49:51 -0600

    added list

--- controllers/teams.js
+++ controllers/teams.js
@@ -1,5 +1,5 @@
-const team = require('../models/team');
 const Team = require('../models/team');
+const User = require('../models/user');
 
 module.exports = {
     index,
@@ -33,9 +33,20 @@ function create(req, res) {
 
 async function deleteTeam(req, res) {
     try {
-      const foundTeam = await team.findById(req.params.id)  
+      const foundTeam = await Team.findById(req.params.id).populate({path:'user'}) 
+      const user = await User.findById(foundTeam.user._id)
+      const teamIndex = user.teams.indexOf(foundTeam._id)
+      user.teams.splice(teamIndex,1)
+      await Team.findByIdAndDelete(req.params.id)
+      await user.save()
     } catch (error) {
         console.log(error) 
         
     }
 }
+
+//update team function
+//controller team function to pass information to create a new game
+//bootstrap nav bar and coloring
+//populate paths game / for each in a show page of each team/ index function but for show & find by id
+//.put route to update team, .post to create game /game/new, .get to render show page
